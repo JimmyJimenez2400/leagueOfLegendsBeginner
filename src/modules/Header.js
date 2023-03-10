aacd54e7422b22aa8ef1f81c84674177f04b7483
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-03-10 15:44:53 -0500

    Add Resouces For Img/Video, Header, Main

---
+++ src/modules/Header.js
@@ -0,0 +1,25 @@
+export default function Header() {
+  return (
+    <section id="header">
+      <nav className="navbar">
+        <section className="img">
+          <img src="" alt="Logo" />
+        </section>
+        <ul className="header-list">
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Learn</a>
+          </li>
+          <li>
+            <a href="">Champions</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+}
